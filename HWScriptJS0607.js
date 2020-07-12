f2b2b7bbc5ec3bdcffe20be78ee0d5335e1167d2
Your Name <you@example.com>
2020-07-12 20:32:11 +0300

    Залил самостоятельно работу на Git Hub

---
+++ HWScriptJS0607.js
@@ -0,0 +1,7 @@
+'use strict';
+window.addEventListener('load', () => {
+
+    const controller = new Controller('login', 'pass1', 'pass2', 'email', 'form1');
+    controller.activateListeners();
+
+});
